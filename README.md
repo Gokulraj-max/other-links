62886652e2f5da296e7c29a69f7bca83a3c45ddf
Gowtham Raj <gowthamrajsivakumar@gmail.com>
2024-11-23 12:26:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Social profile web-app 🔗
+
+This is a web-app that shows my social media links.
+
+![LINK](https://github.com/emapeire/link/assets/63935846/7e0511a6-789f-4156-8b15-41d7dc59e0b2)
+
+###### ✨ Huge thanks to [Lee Robinson](https://github.com/leerob)
---
+++ src/data/social_data.tsx
@@ -0,0 +1,69 @@
+import { DiscordIcon } from '@/components/icons/discord'
+import { GithubIcon } from '@/components/icons/github'
+import { LinkedinIcon } from '@/components/icons/linkedin'
+import { MailIcon } from '@/components/icons/mail'
+
+import { ResumeIcon } from '@/components/icons/resume'
+import { TelegramIcon } from '@/components/icons/telegram'
+import { WebIcon } from '@/components/icons/web'
+import { WhatsappIcon } from '@/components/icons/whatsapp'
+import { XIcon } from '@/components/icons/x'
+
+export const SOCIAL_DATA = {
+  name: 'Govindh',
+  initials: 'GR',
+  avatar: '/assets/avatar.webp',
+  about: 'Aspiring Full-Stack Developer | Design and Innovation Enthusiast',
+  contacts: [
+    {
+      title: 'Portfolio',
+      url: '',
+      icon: WebIcon
+    },
+    {
+      title: 'Resume',
+      url: '',
+      icon: ResumeIcon
+    },
+    {
+      title: 'Email',
+      url: 'mailto:grajbits@gmail.com',
+      icon: MailIcon
+    },
+    {
+      title: 'WhatsApp',
+      url: 'https://wa.me/+918531041822',
+      icon: WhatsappIcon
+    }
+  ],
+  socials: [
+    {
+      title: 'GitHub',
+      url: 'https://github.com/Gokulraj-max',
+      icon: GithubIcon
+    },
+    {
+      title: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/gokul-raj-089682249/',
+      icon: LinkedinIcon
+    },
+    {
+      title: 'The everything app',
+      url: 'https://x.com/grajbits',
+      icon: XIcon
+    },
+    {
+      title: 'Telegram',
+      url: 'https://t.me/Gokulraj787',
+      icon: TelegramIcon
+    }
+  ],
+  communities: [
+
+    {
+      title: 'Discord Community',
+      url: 'https://dub.sh/',
+      icon: DiscordIcon
+    }
+  ]
+}
